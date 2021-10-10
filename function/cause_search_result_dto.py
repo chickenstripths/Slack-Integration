88881c79bc50ba7c0052142225a5763508238cf6
Andy Zhao <azhao24@hotmail.com>
2021-10-10 14:06:02 -0400

    starter function directory

---
+++ function/cause_search_result_dto.py
@@ -0,0 +1,11 @@
+class CauseSearchResultDto:
+    def __init__(self):
+        mygoodness_url = None
+        first_name_global = None
+        result = None
+        second_name_global = None
+        second_result = None
+        second_mygoodness_url = None
+        third_name_global = None
+        third_result = None
+        third_mygoodness_url = None
